c8e67af9f12502d30315ba87f1df60652891ad07
YASHasvi SHUkla <yashasvigot@gmail.com>
2024-04-04 23:32:50 +0530

    done done done done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Mane from './components/Main/Mane'
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Mane />
+    </>
+  )
+}
+
+export default App
